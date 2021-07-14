7f7774170a291f23bd7144055a018df7c673f05c
Igor Silva <igorbr2018@gmail.com>
2021-07-14 14:16:32 -0300

    resolve indentation file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,26 +4,25 @@ import morgan from 'morgan'
 
 import routes from './routes'
 
-
 class Application {
- public express: express.Application
+	public express: express.Application
 
-  constructor() {
-    this.express = express()
-    this.middlewares()
-    this.routes()
-  }
+	constructor() {
+		this.express = express()
+		this.middlewares()
+		this.routes()
+	}
 
-  private middlewares(): void {
-    this.express.use(cors())
-    this.express.use(express.json())
-    this.express.use(express.urlencoded({extended: true}))
-    this.express.use(morgan('dev'))
-  }
+	private middlewares(): void {
+		this.express.use(cors())
+		this.express.use(express.json())
+		this.express.use(express.urlencoded({ extended: true }))
+		this.express.use(morgan('dev'))
+	}
 
-  private routes(): void {
-    this.express.use('/v1', routes)
-  }
+	private routes(): void {
+		this.express.use('/v1', routes)
+	}
 }
 
 export default new Application().express
