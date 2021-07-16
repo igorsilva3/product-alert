ca2d6a726c764ddb671dc1c41f022dbc85a33ef7
Igor Silva <igorbr2018@gmail.com>
2021-07-16 11:49:46 -0300

    Updated for new configs

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,4 +1,9 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	clearMocks: true,
+	collectCoverage: true,
+	bail: true,
+	testMatch: ['**/__tests__/**/*.spec.ts'],
+	coverageDirectory: './__tests__/coverage',
+}
