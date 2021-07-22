df0750ebde00072af20bd3d87eca4d90d1eb2aa2
Igor Silva <igorbr2018@gmail.com>
2021-07-21 23:47:46 -0300

    Create feature for users

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express'
-import { PrismaClient, Product } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import * as Yup from 'yup'
 import { hash } from 'bcrypt'
 
 const prisma = new PrismaClient()
-
 const schema = Yup.object().shape({
 	firstName: Yup.string().required('O campo nome é obrigatório'),
 	lastName: Yup.string().required('O campo sobrenome é obrigatório'),
 	email: Yup.string().required('O campo email é obrigatório'),
 	password: Yup.string().required('O campo senha é obrigatório'),
+	createdAt: Yup.date(),
+	updatedAt: Yup.date(),
 })
-
 class UserController {
 	public async index(request: Request, response: Response) {
 		try {
@@ -36,6 +36,33 @@ class UserController {
 		}
 	}
 
+	public async show(request: Request, response: Response) {
+		try {
+			const { id } = request.params
+
+			const user = await prisma.user.findUnique({
+				where: {
+					id,
+				},
+				select: {
+					id: true,
+					firstName: true,
+					lastName: true,
+					email: true,
+					createdAt: true,
+					updatedAt: true,
+					products: true,
+				},
+			})
+
+			return response
+				.status(200)
+				.json(user === null ? { message: 'User not found' } : user)
+		} catch (error) {
+			return response.status(400).json({ error })
+		}
+	}
+
 	public async create(request: Request, response: Response) {
 		try {
 			const { firstName, lastName, email, password } = request.body
@@ -44,7 +71,7 @@ class UserController {
 				firstName,
 				lastName,
 				email,
-				password: await hash(password, 32),
+				password: await hash(password, 10),
 			}
 
 			await schema.validate(data, {
@@ -64,11 +91,68 @@ class UserController {
 				},
 			})
 
-			return response.status(200).json(user)
+			return response.status(201).json(user)
 		} catch (error) {
 			return response.status(400).json({
 				error: error instanceof Yup.ValidationError ? error.errors : error,
 			})
+		}
+	}
+
+	public async update(request: Request, response: Response) {
+		try {
+			const { id } = request.params
+
+			const { firstName, lastName, email, password } = request.body
+
+			const data = {
+				firstName,
+				lastName,
+				email,
+				password: await hash(password, 10),
+				updatedAt: new Date().toISOString(),
+			}
+
+			await schema.validate(data, {
+				abortEarly: false,
+			})
+
+			const user = await prisma.user.update({
+				where: { id },
+				data,
+				select: {
+					id: true,
+					firstName: true,
+					lastName: true,
+					email: true,
+					createdAt: true,
+					updatedAt: true,
+					products: true,
+				},
+			})
+
+			return response.status(200).json(user)
+		} catch (error) {
+			console.error(error)
+			return response.status(400).json({
+				error: error instanceof Yup.ValidationError ? error.errors : error,
+			})
+		}
+	}
+
+	public async delete(request: Request, response: Response) {
+		try {
+			const { id } = request.params
+
+			const user = await prisma.user.delete({
+				where: { id },
+			})
+
+			console.log(user)
+
+			return response.status(200).json({ message: 'User deleted successful' })
+		} catch (error) {
+			return response.status(400).json({ error })
 		}
 	}
 }
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -5,5 +5,9 @@ import { UserController  } from '@controllers'
 const router = Router()
 
 router.get('/', UserController.index)
+router.get('/:id', UserController.show)
+router.post('/', UserController.create)
+router.put('/:id', UserController.update)
+router.delete('/:id', UserController.delete)
 
 export default router
