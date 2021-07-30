f26582ca8cdb658126e239e9226dce118ecd86a6
Igor Silva <igorbr2018@gmail.com>
2021-07-30 12:02:42 -0300

    Completed tests for product

--- backend/__tests__/integration/products.spec.ts
+++ backend/__tests__/integration/products.spec.ts
@@ -1,30 +1,38 @@
-import { Product } from '@prisma/client'
 import faker from 'faker'
 import { agent } from 'supertest'
 import app from '../../src/app'
+import { createProduct, createUser } from '../utils/factories'
 
 const request = agent(app)
 
-const createProduct = (): Product => ({
-	id: faker.datatype.uuid(),
-	name: faker.commerce.productName(),
-	company: faker.company.companyName(),
-	amount: faker.datatype.number(),
-	expirationDate: faker.date.future(),
-	createdAt: faker.date.recent(),
-	updatedAt: faker.date.recent(),
-  userId: faker.datatype.uuid()
-})
+const user = createUser()
+const product = createProduct({ userId: user.id })
 
 describe('Router products', () => {
-  it('should create a product', async () => {
-  })
-  it('should to list all products created', async () => {
+	it('should create a user', async () => {
+		const { statusCode } = await request.post('/v1/users').send(user)
+		expect(statusCode).toEqual(201)
+	})
+	it('should create a product', async () => {
+		const { statusCode } = await request.post('/v1/products').send(product)
+		expect(statusCode).toEqual(201)
+	})
+	it('should to list all products created', async () => {
+		const { statusCode } = await request.get('/v1/products/')
+		expect(statusCode).toEqual(200)
 	})
 	it('should to list a product by passing id', async () => {
+		const { statusCode } = await request.get(`/v1/products/${product.id}`)
+		expect(statusCode).toEqual(200)
 	})
 	it('should to update a product by passing id', async () => {
+		product.amount = faker.datatype.number()
+
+		const { statusCode } = await request.put(`/v1/products/${product.id}`).send(product)
+		expect(statusCode).toEqual(200)
 	})
 	it('should to delete a product', async () => {
+		const { statusCode } = await request.delete(`/v1/products/${product.id}`)
+		expect(statusCode).toEqual(200)
 	})
 })
--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -2,16 +2,123 @@ import { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 import * as Yup from 'yup'
 
+const prisma = new PrismaClient()
+
+const schema = Yup.object().shape({
+	name: Yup.string().required('O campo nome é obrigatório'),
+	company: Yup.string().required('O campo companhia é obrigatório'),
+	expirationDate: Yup.string().required(
+		'O campo data de expiração é obrigatório'
+	),
+	amount: Yup.number().required('O campo quantidade é obrigatório'),
+	userId: Yup.string().required('O campo userId é obrigatório'),
+	createdAt: Yup.date(),
+	updatedAt: Yup.date(),
+})
 class ProductController {
-	public async index(request: Request, response: Response) {}
+	public async index(request: Request, response: Response) {
+		try {
+			const products = await prisma.product.findMany({
+				orderBy: {
+					expirationDate: 'asc',
+				},
+			})
 
-	public async show(request: Request, response: Response) {}
+			return response.status(200).json(products)
+		} catch (error) {
+			return response.status(400).json({ error })
+		}
+	}
 
-	public async create(request: Request, response: Response) {}
+	public async show(request: Request, response: Response) {
+		try {
+			const { id } = request.params
+			const product = await prisma.product.findUnique({
+				where: { id },
+			})
 
-	public async update(request: Request, response: Response) {}
+			if (product === null)
+				return response.status(404).json({ message: 'User not found' })
 
-	public async delete(request: Request, response: Response) {}
+			return response.status(200).json(product)
+		} catch (error) {
+			return response.status(400).json({ error })
+		}
+	}
+
+	public async create(request: Request, response: Response) {
+		try {
+			const { id, name, company, expirationDate, amount, userId } = request.body
+
+			const data = {
+				id: !id ? undefined : id,
+				name,
+				company,
+				expirationDate,
+				amount,
+				userId,
+			}
+
+			await schema.validate(data, {
+				abortEarly: false,
+			})
+
+			const product = await prisma.product.create({
+				data,
+			})
+
+			return response.status(201).json(product)
+		} catch (error) {
+			return response.status(400).json({
+				error: error instanceof Yup.ValidationError ? error.errors : error,
+			})
+		}
+	}
+
+	public async update(request: Request, response: Response) {
+		try {
+			const { name, company, expirationDate, amount, userId } = request.body
+			const { id } = request.params
+
+			const data = {
+				name,
+				company,
+				expirationDate,
+				amount,
+				userId,
+				updatedAt: new Date().toISOString(),
+			}
+
+			await schema.validate(data, {
+				abortEarly: false,
+			})
+
+			const product = await prisma.product.update({
+				where: { id },
+				data,
+			})
+
+			return response.status(200).json(product)
+		} catch (error) {
+			return response.status(400).json({
+				error: error instanceof Yup.ValidationError ? error.errors : error,
+			})
+		}
+	}
+
+	public async delete(request: Request, response: Response) {
+		try {
+			const { id } = request.params
+
+			await prisma.product.delete({
+				where: { id },
+			})
+
+			return response.status(200).json({ message: 'Product deleted successful' })
+		} catch (error) {
+			return response.status(400).json({ error })
+		}
+	}
 }
 
 export default new ProductController()
