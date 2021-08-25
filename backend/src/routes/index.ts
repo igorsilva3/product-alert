c969aaa3497b229b9622427af0d6d0fb0f9ef110
Igor Silva <igorbr2018@gmail.com>
2021-08-25 19:31:02 -0300

    Created router for auth

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import userRouter from '@routes/userRouter'
 import productRouter from '@routes/productRouter'
+import authRouter from '@routes/authRouter'
 
 const router = Router()
 
 router.use('/users', userRouter)
 router.use('/products', productRouter)
+router.use('/auth', authRouter)
 
 export default router
