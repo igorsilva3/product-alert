345a7ff300d6e685e01540a68f17a48356ca5c77
Igor Silva <igorbr2018@gmail.com>
2021-10-04 00:57:14 -0300

    Created test unit

--- backend/__tests__/unit/auth.spec.ts
+++ backend/__tests__/unit/auth.spec.ts
@@ -1,35 +1,37 @@
-import { agent } from 'supertest'
-import app from '../../src/app'
-import { createUser } from '../utils/factories'
+import { AuthService } from '../../src/services/AuthService'
 
-const request = agent(app)
+const authService = new AuthService()
 
-const user = createUser()
+const user = {
+	email: 'igor@mail.com',
+	password: '123456abcd',
+	token: ''
+}
 
 describe('Authenticate a user', () => {
-	it('should create a user', async () => {
-		const { statusCode } = await request.post('/v1/users').send(user)
-		expect(statusCode).toEqual(201)
-	})
 	it('should get a token', async () => {
-		const { statusCode, body } = await request.post('/v1/auth/').send({
-			email: user.email,
-			password: user.password,
-		})
+		const { user: User, token } = await authService.signin(user.email, user.password)
+		
+		user.token = token
 
-		expect({ statusCode, body }).toEqual({
-			statusCode: 200,
-			body: {
-				user: {
-					id: expect.any(String),
-					firstName: expect.any(String),
-					lastName: expect.any(String),
-					email: expect.any(String),
-					createdAt: expect.any(String),
-					updatedAt: expect.any(String),
-				},
-				token: expect.any(String),
+		expect({ User, token }).toEqual({
+			User: {
+				id: expect.any(String),
+				firstName: expect.any(String),
+				lastName: expect.any(String),
+				email: expect.any(String),
+				createdAt: expect.any(Date),
+				updatedAt: expect.any(Date),
 			},
+			token: expect.any(String),
+		})
+	}),
+	it('should verify a token', async () => {
+		const decoded = await authService.decode(user.token)
+		expect(decoded).toEqual({
+			userId: expect.any(String),
+			iat: expect.any(Number),
+			exp: expect.any(Number),
 		})
 	})
 })
--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import faker from 'faker'
 import { hash } from 'bcrypt'
 
 const prisma = new PrismaClient()
@@ -8,11 +7,17 @@ async function main() {
   // Create a user
   const password = '123456abcd'
 
-  await prisma.user.create({
-    data: {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
+  await prisma.user.upsert({
+    where: {
+      email: 'igor@mail.com'
+    },
+    update: {
+      updatedAt: new Date()
+    },
+    create: {
+      firstName: 'Igor',
+      lastName: 'Silva',
+      email: 'igor@mail.com',
       password: await hash(password, 10)
     }
   })
